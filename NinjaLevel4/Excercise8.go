ba94a25d313f38be245f04308daf19b2c7ef3786
Alex Escobar <alexdescobars@gmail.com>
2021-02-08 18:02:51 +0100

    Excercise 8 from NinjaLevel4 Completed

--- NinjaLevel4/Excercise8.go
+++ NinjaLevel4/Excercise8.go
@@ -2,8 +2,40 @@
 
 package main
 
+import "fmt"
+
 func main() {
 
-	m := make(map[string][]string)
+	m := make(map[string][]string) // MAP of string TO slice of string
+
+	// This is how not do assign vales to the map: https://play.golang.org/p/-fudOVE4wCV
+
+	// These are multiple ways to do it:
+
+	// https://play.golang.org/p/RHHvMmW675R
+
+	// https://play.golang.org/p/46vg1kr-91n
+
+	// https://play.golang.org/p/T0GW40KWgqz
+
+	m["Mr Deadpool"] = []string{"killing", "shooting", "stabbing"}
+
+	m["Jean Grey"] = []string{"helping", "teaching", "burning"}
+
+	m["Quicksilver"] = []string{"running", "stealing", "joking"}
+
+	fmt.Printf("\tTHIS IS THE COMPLETE MAP: %v\n", m)
+
+	fmt.Println(" ")
+
+	for key, value := range m {
+
+		fmt.Printf("KEY: %v\t\n", key)
+
+		for _, s := range value {
+
+			fmt.Printf("\t\t\tVALUE: %v\t\n", s)
+		}
+	}
 
 }
