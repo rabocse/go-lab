82a778d1c00e1f159df6ea275e2828f88ebbf501
Alex Escobar <alexdescobars@gmail.com>
2021-03-10 07:10:43 +0100

    Changed Excercise9 - Callback

--- NinjaLevel6/Excercise9.go
+++ NinjaLevel6/Excercise9.go
@@ -4,21 +4,41 @@ import (
 	"fmt"
 )
 
-func duplicatePrinting(f func()) (a func(), b func()) {
+// "foo" accepts two arguments:
+// 1 - a funcion like "f" like func(xi []int) int
+// 2 - a slice of int called "ii"
+// finally, "foo" returns an int.
 
-	var func1 func() = f
-	var func2 func() = f
+func foo(f func(xi []int) int, ii []int) int {
 
-	return func1, func2
-
+	n := f(ii) // Notice that I cannot predict the exact result from here since foo will receive a function "f" that can do whatever thing.
+	// What we can expect from here is that whatever int the return from "f" is, "foo" will increment this and return it.
+	n++
+	return n
 }
-
-var fx func() = fmt.Println("test")
 
 func main() {
 
-	a, b := duplicatePrinting(fx)
+	g := func(xi []int) int {
+
+		if len(xi) == 0 {
+
+			return 0
+		}
+		if len(xi) == 1 {
+			return xi[0]
+		}
+		return xi[0] + xi[len(xi)-1] // Given an slice "xi", "g" function will return the sum of the first and last value.
+
+	}
+
+	x := foo(g, []int{1, 2, 3, 4, 5}) // "g" and a slice are passed to "foo".
+	// then, "foo" calls the same "g" and passes the slice to it.
+	// then "g" returns the sum of 1 and 5 as those are the first and last values from the passed slice.
+	// finally, "foo" takes (1 + 5) as return from "g" and increments 1 to it to then return it (7).
+	fmt.Println(x)
 
 }
 
-// Wrong  ! Need to fix this.
+// This is what is called a callback. A function that calls a function as parameter.
+// Functions are "first class citizens" in Go.
