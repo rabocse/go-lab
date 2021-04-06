6bb92c07e64f9c45d9cc9e242b5e8121a75dd4db
Alex Escobar <alexdescobars@gmail.com>
2021-04-06 21:07:33 +0200

    Factorial working and panicking when needed. To fix: proper finishing after valid factorial calculation

--- Foundation/Factorial/Factorial.go
+++ Foundation/Factorial/Factorial.go
@@ -5,40 +5,7 @@ import (
 	"strconv"
 )
 
-func main() {
-
-	// Request the number:
-	fmt.Print("Enter a positive number: ")
-
-	// Run loop to read the number:
-	for {
-		var input string
-		fmt.Scanln(&input)
-
-		// Convert the input (string) from user to integer:
-		inputInt, err := strconv.Atoi(input)
-		if err != nil {
-			fmt.Println("error: ", err)
-		}
-		// Validate that the user entered a value
-		if len(input) == 0 {
-			print("Please, enter a value")
-			break
-		}
-		if inputInt <= 0 || inputInt > 100 {
-			panic("PANIC !!!!!!!!")
-		} else {
-			myfactorial(inputInt)
-			}
-			fmt.Print(factorial)
-		}
-
-	}
-}
-
-
-
-func myfactorial(inputInt int) uint64 {  
+func myfactorial(inputInt int) int {
 
 	if inputInt <= 0 || inputInt > 100 {
 		panic("PANIC !!!!!!!!")
@@ -50,4 +17,35 @@ func myfactorial(inputInt int) uint64 {
 		return factorial
 	}
 }
-	
+
+func main() {
+
+	// Request the number:
+	fmt.Print("Enter a positive number: ")
+
+	// Run loop to read the number:
+	for {
+		var input string
+		fmt.Scanln(&input)
+
+		// Validate that the user entered a value:
+		if len(input) == 0 {
+			print("Value was not entered. Exiting program...")
+			break
+		}
+		// Convert the input (string) from user to integer and handle the error:
+		inputInt, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Converting from string to int failed:", err)
+		}
+
+		// Now that inputInt is used to define whether is valid value to calculate factorial:
+		//		if inputInt <= 0 || inputInt > 100 {
+		//			panic("PANIC !!!!!!!!")
+
+		fact := myfactorial(inputInt)
+		fmt.Print(fact)
+
+		// fmt.Print(factorial)
+	}
+}
