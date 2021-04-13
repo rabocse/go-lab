1c950512b9f46524917ca7b3564db8a0a370007a
Alex Escobar <alexdescobars@gmail.com>
2021-04-13 16:15:47 +0200

    Fixed a type on WebResourcePaths.go. I was using Reponse instead of Response

--- Courses/WebResourcePaths.go
+++ Courses/WebResourcePaths.go
@@ -1,43 +1,36 @@
 package main
 
 import (
-
 	"log"
 	"net/http"
 )
 
+func write(writer http.ResponseWriter, message string) {
+	_, err := writer.Write([]byte(message))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func write(writer http.ReponseWriter, message string) {
-_, err := writer.Write([]byte(message))
-if err != nil {
-	log.Fatal(err)
 }
-
-}
-
 
 func englishHandler(writer http.ResponseWriter, request *http.Request) {
-write(writer, "Hello Web !")
+	write(writer, "Hello Web !")
 }
-
 
 func spanishHandler(writer http.ResponseWriter, request *http.Request) {
 	write(writer, "Hola web !")
 }
 
-
-func polishHandler(writer http.ResponseWrite, request *http.Request) {
+func polishHandler(writer http.ResponseWriter, request *http.Request) {
 	write(writer, "cześć")
 }
 
-
-
 func main() {
 
-http.HandleFunc("/eng", englishHandler)
-http.HandleFunc("/es", spanishHandler)
-http.HandleFunc("/pl", polishHandler)
-err := http.ListenAndServe("localhost:8080", nil)
-log.Fatal(err)
+	http.HandleFunc("/eng", englishHandler)
+	http.HandleFunc("/es", spanishHandler)
+	http.HandleFunc("/pl", polishHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
 
 }
